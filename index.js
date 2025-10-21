586efefcd92d87d15a58688f9f8ce3f057b5672b
danny fadli <danyfdly95@gmail.com>
2025-10-21 14:21:43 +0700

    menambahkan fungsi koneksi MySQL dengan pengecekan error dan log status koneksi berhasil.

--- index.js
+++ index.js
@@ -21,3 +21,10 @@ const db = mysql.createConnection({
   port: 3309
 });  
 
+db.connect((err) => {
+  if (err) {
+    console.error('Error connecting to the database:', err);
+    return;
+  }
+    console.log('Connected to the MySQL database.');    
+});
