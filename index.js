f017a448830b52fdf1f6467d3e0130230c478e4f
danny fadli <danyfdly95@gmail.com>
2025-10-21 14:20:47 +0700

    setup Express server and MySQL connection

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
+
+const db = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '15975321dny',
+  database: 'mahasiswa',
+  port: 3309
+});  
+
