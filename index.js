82e5abbfca42d4a3db615fcbb13577ae5c28f90d
danny fadli <danyfdly95@gmail.com>
2025-10-21 14:16:50 +0700

    fitur: buat route utama '/' dengan respons Hello World

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ const express = require('express');
 let mysql = require('mysql2');
 const app = express();
 const port = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
